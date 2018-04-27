2b4d6a02a7e4f28fc83c701d2f13d5a82fa73986
Kiril Dokh <dsounded@gmail.com>
2018-04-27 13:41:52 +0300

    optimzization

--- app/utils/notify.rb
+++ app/utils/notify.rb
@@ -6,13 +6,15 @@ class Notify
   attr_accessor :subscribers
 
   def each(&block)
-    subscribers.to_a.in_groups_of(SUBSCRIBERS_BATCH_SIZE, false).each do |batch_subscribers|
+    batches = subscribers.to_a.in_groups_of(SUBSCRIBERS_BATCH_SIZE, false)
+    count = batches.count
+    batches.each_with_index do |batch_subscribers, i|
       Thread.new do
         batch_subscribers.each do |subscriber|
           block.call(subscriber) rescue nil # in case someone blocked bot
         end
       end
-      sleep(1) # prevents >30 messages per seconds(not allowed by Telegram API)
+      sleep(1) unless i == count - 1 # prevents >30 messages per seconds(not allowed by Telegram API)
     end
   end
 end
